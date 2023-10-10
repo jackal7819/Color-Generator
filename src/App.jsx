6caa3f3418f540c108e576cfd2353aba773e1656
Viktor Filippov <jackal3166@gmail.com>
2023-10-10 14:13:19 +0300

    Add Values

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import ColorList from './ColorList';
 import Form from './Form';
+import Values from 'values.js';
+import { useState } from 'react';
 
 const App = () => {
+    const [colors, setColors] = useState(new Values('#8125f1').all(10));
+    
     return (
         <main>
             <Form />
