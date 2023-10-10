598ed98525b37107223314ed8e2b927ab5e7e741
Viktor Filippov <jackal3166@gmail.com>
2023-10-10 14:04:27 +0300

    Create Form

---
+++ src/Form.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+const Form = () => {
+    const [color, setColor] = useState('');
+
+    const handleSubmit = (event) => {};
+
+    return (
+        <section className='container'>
+            <h4>color generator</h4>
+            <form className='color-form' onSubmit={handleSubmit}>
+                <input
+                    type='color'
+                    value={color}
+                    onChange={(event) => setColor(event.target.value)}
+                />
+                <input
+                    type='text'
+                    value={color}
+                    placeholder='#8125f1'
+                    onChange={(event) => setColor(event.target.value)}
+                />
+                <button
+                    type='submit'
+                    className='btn'
+                    style={{ background: color }}>
+                    submit
+                </button>
+            </form>
+        </section>
+    );
+};
+
+export default Form;
